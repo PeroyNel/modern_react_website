63460964f23e3211303cd5582c743d548a2e44b7
Peroy Nel <peroynel@yahoo.com>
2022-01-14 14:10:48 +0200

    CSS Updates to App.js
    CSS Updates to App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { Footer, Blog, Possibility, Features, What, Header } from './containers';
 import { CTA, Brand, Navbar } from './components';
+import './App.css';
 
 
 const App = () => {
--- src/containers/what/What.jsx
+++ src/containers/what/What.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import 'what.css';
+import './what.css';
 
 const What = () => {
     return (
