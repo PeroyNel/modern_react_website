b79de1be9a5044aa285ef34b4d55e67ffc80ae57
Peroy Nel <peroynel@yahoo.com>
2022-01-24 16:27:47 +0200

    Navbar Component Added
    Navbar Component Added

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import './navbar.css';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+import './navbar.css';
+import logo from '../../assets/logo.svg';
 
 const Navbar = () => {
     return (
-        <div>
-            Navbar
+        <div className="gpto3__navbar">
+            <div className="gpto3__navbar-links">
+                <div className="gtp3__navbar-links_logo">
+                    <img src={logo} alt="logo" />
+                </div>
+                <div className="gtp3__navbar-links_container">
+                    <p><a href="#home">Home</a></p>
+                    <p><a href="#wgpt3">What is GPT3?</a></p>
+                    <p><a href="#possibility">Open AI</a></p>
+                    <p><a href="#features">Case Studies</a></p>
+                    <p><a href="#blog">Library</a></p>
+
+                </div>
+            
+            </div>
         </div>
     )
 }
