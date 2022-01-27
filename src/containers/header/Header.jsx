fc25158951f672af1334f1cfea8194b897f02189
Peroy Nel <peroynel@yahoo.com>
2022-01-27 15:31:45 +0200

    Header Component Elements and Styling
    Header Component Elements and Styling

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -13,14 +13,15 @@ const Header = () => {
                     <input type="email" placeholder="Your Email Address"></input>
                     <button type="button">Get Started</button>
                 </div>
-                <div className="gpt3__header-content__people-list">
+                <div className="gpt3__header-content__people">
                     <img src={people} alt="people"></img>
                     <p>1,600 people requested access a visit in last 24 hours</p>
                 </div>
 
-                <div className="gpt3__header-image">
-                    <img src={ai} alt="ai"></img>
-                </div>
+                
+            </div>
+            <div className="gpt3__header-image">
+                <img src={ai} alt="ai"></img>
             </div>
         </div>
     )
