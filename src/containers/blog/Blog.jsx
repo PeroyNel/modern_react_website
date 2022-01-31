ed49367c4c675ba11057b6dee86dad8a3f1e15e5
Peroy Nel <peroynel@yahoo.com>
2022-01-31 16:30:44 +0200

    Blog Container Updates and Article Component Implementation and Styling
    Blog Container Updates and Article Component Implementation and Styling

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './blog.css';
 import { Article } from '../../components';
+import { blog01, blog02, blog03, blog04, blog05 } from './imports';
 
 const Blog = () => {
     return (
@@ -11,14 +12,14 @@ const Blog = () => {
             </div>
             <div className="gpt3__blog-container">
                 <div className="gpt3__blog-container_groupA">
-                    <Article />
+                    <Article imgUrl={blog01} date="Sep 26, 2021" title="GPT-3 and Open  AI is the future. Let us exlore how it is?"/>
 
                 </div>
                 <div className="gpt3__blog-container_groupB">
-                    <Article />
-                    <Article />
-                    <Article />
-                    <Article />
+                    <Article imgUrl={blog02} date="Sep 26, 2021" title="GPT-3 and Open  AI is the future. Let us exlore how it is?"/>
+                    <Article imgUrl={blog03} date="Sep 26, 2021" title="GPT-3 and Open  AI is the future. Let us exlore how it is?"/>
+                    <Article imgUrl={blog04} date="Sep 26, 2021" title="GPT-3 and Open  AI is the future. Let us exlore how it is?"/>
+                    <Article imgUrl={blog05} date="Sep 26, 2021" title="GPT-3 and Open  AI is the future. Let us exlore how it is?"/>
 
 
                 </div>
